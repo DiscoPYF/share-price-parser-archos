824f16859af61cac501280c41478bb7aac65d555
Pierre-Yves FARE <pyfare@gmail.com>
2015-12-18 07:47:29 +0100

    Delimiter correction and format for volume value

--- ArchosSharePriceParser/Program.cs
+++ ArchosSharePriceParser/Program.cs
@@ -30,7 +30,7 @@ namespace ArchosSharePriceParser
                     using (TextFieldParser parser = new TextFieldParser(options.InputFilePath))
                     {
                         parser.CommentTokens = new string[] { "#" };
-                        parser.SetDelimiters(new string[] { ";" });
+                        parser.SetDelimiters(new string[] { "," });
                         parser.HasFieldsEnclosedInQuotes = false;
                         parser.TextFieldType = FieldType.Delimited;
 
@@ -54,8 +54,7 @@ namespace ArchosSharePriceParser
 
         public static void ParseAverage(TextFieldParser parser)
         {
-            string line;
-            string[] values;
+            string[] fields;
             DateTime lastDateTime;
             DateTime dateTime;
             AverageEntry averageEntry;
@@ -68,15 +67,14 @@ namespace ArchosSharePriceParser
             info = new CultureInfo("en");
             while (!parser.EndOfData)
             {
-                line = parser.ReadLine();
-                values = line.Split(',');
+                fields = parser.ReadFields();
 
-                averageEntry.OpenPrice += float.Parse(values[1], info);
-                averageEntry.ClosePrice += float.Parse(values[2], info);
-                averageEntry.HighPrice += float.Parse(values[3], info);
-                averageEntry.LowPrice += float.Parse(values[4], info);
-                averageEntry.Volume += int.Parse(values[5], info);
-                dateTime = DateTime.Parse(values[0]);
+                averageEntry.OpenPrice += float.Parse(fields[1], info);
+                averageEntry.ClosePrice += float.Parse(fields[2], info);
+                averageEntry.HighPrice += float.Parse(fields[3], info);
+                averageEntry.LowPrice += float.Parse(fields[4], info);
+                averageEntry.Volume += int.Parse(fields[5], info);
+                dateTime = DateTime.Parse(fields[0]);
 
                 // Counter has been reset ? (To prevent writing on the very first cycle)
                 if (i == 1)
@@ -88,7 +86,7 @@ namespace ArchosSharePriceParser
                 {
                     averageEntry.Month = dateTime.Month;
                     averageEntry.Year = dateTime.Year;
-                    Console.WriteLine(string.Format("{0}-{1},{2},{3},{4},{5}", averageEntry.Month, averageEntry.Year,
+                    Console.WriteLine(string.Format(info, "{0}-{1},{2},{3},{4},{5},{6}", averageEntry.Month, averageEntry.Year,
                                                                                averageEntry.OpenPrice / i, averageEntry.ClosePrice / i,
                                                                                averageEntry.HighPrice / i, averageEntry.LowPrice / i,
                                                                                averageEntry.Volume / i));
